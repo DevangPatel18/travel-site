0f225a198e948cd1d18615ee335628a63f6524ce
Dev <devang713@gmail.com>
2018-03-27 22:50:17 -0400

    Complete desktop and mobile header styles (correct)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,3 @@
-var $ = require('jquery');
+import MobileMenu from './modules/MobileMenu';
 
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
-
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes million");
-	}
-}
-
-alert("This is warnings!");
-
-var john = new Person('john');
-john.greet();
-
-var jane = new Adult('jane');
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
